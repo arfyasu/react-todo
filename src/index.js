f703422e86ad44d36e8e0e62aa7ad0b9649d6bae
Yasutaka Kanzaki <yasutaka.kanzaki@gmail.com>
2016-02-17 22:08:20 +0900

    added addTask action.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 var TaskList = React.createClass({
   getInitialState() {
     return {
-      tasks: [
-        {id: 1, name: "hoge"}
-      ]
+      tasks: [],
+      nextId: 1
     };
+  },
+  addTask(taskName) {
+    var newTask = {id: this.state.nextId, name: taskName};
+    this.setState({
+      tasks: this.state.tasks.concat([newTask]),
+      nextId: this.state.nextId + 1
+    });
+  },
+  editTask(id) {
+    // TODO
+  },
+  finishTask(id) {
+    // todo
   },
   render() {
     var tasks = this.state.tasks.map(task => {
       return <li key={task.id}><Task task={task} /></li>;
     });
     return (
-      <ul>{tasks}</ul>
+      <ul>
+        {tasks}
+        <li><TaskCreator addTask={this.addTask} /></li>
+      </ul>
     );
   }
 });
@@ -30,4 +45,57 @@ var Task = React.createClass({
   }
 });
 
-ReactDOM.render(<TodoList />, document.getElementById('container'));
+var TaskCreator = React.createClass({
+  propTypes: {
+    addTask: React.PropTypes.func.isRequired
+  },
+  getInitialState() {
+    return {
+      edit: false,
+      taskName: ""
+    }
+  },
+  init() {
+    this.setState({
+      edit: false,
+      taskName: ""
+    })
+  },
+  handleClickAddTaskLink() {
+    this.setState({edit: true});
+  },
+  handleClickCancelLink() {
+    this.setState({edit: false});
+  },
+  handleClickAddTaskButton() {
+    var taskName = this.state.taskName;
+    if (taskName.length > 0) {
+      this.props.addTask(taskName);
+      this.init();
+    }
+  },
+  handleChangeTaskNameText(e) {
+    this.setState({taskName: e.target.value});
+  },
+  render() {
+    return (
+      <div>
+        {(() => {
+          if (this.state.edit) {
+            return (
+              <p>
+                <input type="text" value={this.state.taskName} onChange={this.handleChangeTaskNameText}/><br/>
+                <button onClick={this.handleClickAddTaskButton}>Add task</button>
+                <a href="#" onClick={this.handleClickCancelLink}>Cancel</a>
+              </p>
+            );
+          } else {
+            return <p><a href="#" onClick={this.handleClickAddTaskLink}>Add task</a></p>
+          }
+        })()}
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(<TaskList />, document.getElementById('container'));
