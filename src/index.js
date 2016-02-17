8278bbf037d14ad10e5ec997e198771047971282
Yasutaka Kanzaki <yasutaka.kanzaki@gmail.com>
2016-02-17 19:24:40 +0900

    show task list.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
-var TodoList = React.createClass({
+var TaskList = React.createClass({
+  getInitialState() {
+    return {
+      tasks: [
+        {id: 1, name: "hoge"}
+      ]
+    };
+  },
+  render() {
+    var tasks = this.state.tasks.map(task => {
+      return <li key={task.id}><Task task={task} /></li>;
+    });
+    return (
+      <ul>{tasks}</ul>
+    );
+  }
+});
+
+var Task = React.createClass({
+  propTypes: {
+    task: React.PropTypes.shape({
+      id: React.PropTypes.number.isRequired,
+      name: React.PropTypes.string.isRequired
+    })
+  },
   render() {
     return (
-      <div>hello world</div>
+      <p>{this.props.task.name}</p>
     );
   }
 });
