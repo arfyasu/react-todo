897fc235ba462c812ea73bb9f1ca38c87a10bb9f
Yasutaka Kanzaki <yasutaka.kanzaki@gmail.com>
2016-02-17 19:07:26 +0900

    hello world.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+var TodoList = React.createClass({
+  render() {
+    return (
+      <div>hello world</div>
+    );
+  }
+});
+
+ReactDOM.render(<TodoList />, document.getElementById('container'));
